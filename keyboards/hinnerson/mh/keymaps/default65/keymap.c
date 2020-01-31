d2e09480b5df13df3a6da23cbbaea03c260eb32c
Martin Hinnerson <marhi386@gmail.com>
2020-01-31 23:38:56 +0100

    remove caps and add double tap to toggle mouse layer

--- keyboards/hinnerson/mh/keymaps/default65/keymap.c
+++ keyboards/hinnerson/mh/keymaps/default65/keymap.c
@@ -2,24 +2,18 @@
 #include "rev1.h"
 #include "keymap_swedish.h"
 
-// extern keymap_config_t keymap_config;
-
 #define _DEFAULT 0
 #define _MOUSE 1
 #define _MOVE 2
 
-// enum custom_keycodes {
-//   QWERTY = SAFE_RANGE,
-// };
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DEFAULT] = LAYOUT_65(
 //┌────────┬────────┬────────┬────────┬────────┬────────┬────────┐          //┌────────┬────────┬────────┬────────┬────────┬────────┬─────────────────┬────────┐
    KC_GESC, KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_6,                 KC_7,    KC_8,    KC_9,    KC_0,   SE_PLUS,  KC_EQL,      KC_BSPC,     KC_HOME, \
 //|────────┴────┬───┴────┬───┴────┬───┴────┬───┴────┬───┴────┬───┘      //┌───┴────┬───┴────┬───┴────┬───┴────┬───┴────┬───┴────┬───┴────┬────────────┬────────|
     KC_TAB,       KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   SE_AA,  SE_QUOT,              KC_END,  \
 //|─────────────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┐        //└─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┬──────┴─┐          |────────|
-    KC_CAPS,        KC_A,    KC_S,    KC_D,     KC_F,    KC_G,                 KC_H,    KC_J,    KC_K,    KC_L,  SE_OSLH,  SE_AE,  SE_APOS,   KC_ENT,  KC_PGUP, \
+    TT(_MOUSE),     KC_A,    KC_S,    KC_D,     KC_F,    KC_G,                 KC_H,    KC_J,    KC_K,    KC_L,  SE_OSLH,  SE_AE,  SE_APOS,   KC_ENT,  KC_PGUP, \
 //|──────────┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┐      //└───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴────────┴─┬────────|────────|
     KC_LSFT,  SE_LESS,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                 KC_N,    KC_M,  KC_COMM, KC_DOT,  SE_MINS,     KC_RSFT,     KC_UP,  KC_PGDN, \
 //|──────────|────────┴─┬──────┴───┬────┴─────┬──┴────────┴────────|          //|────────┴────────┴──────┬─┴──────┬─┴──────┬─┴──────┬────────|────────|────────|
